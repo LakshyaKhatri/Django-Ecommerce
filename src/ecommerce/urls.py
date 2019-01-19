5b9eb3079e7821ed9c2907778cec17bc35a2d5ad
LakshyaKhatri <lakshyak67@gmail.com>
2019-01-19 19:44:29 +0530

    Urls changed to regex urls

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -17,16 +17,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path
 from .views import home_page, about_page, contact_page, login_page, register_page
+from products.views import product_list_view
 
 urlpatterns = [
-    path('', home_page),
-    path('about/', about_page),
-    path('contact/', contact_page),
-    path('login/', login_page),
-    path('register/', register_page),
-    path('admin/', admin.site.urls),
+    re_path(r'^$', home_page),
+    re_path(r'^about/$', about_page),
+    re_path(r'^contact/$', contact_page),
+    re_path(r'^login/$', login_page),
+    re_path(r'^register/$', register_page),
+    re_path(r'^products/$', product_list_view),
+    re_path(r'^admin/$', admin.site.urls),
 ]
 
 if settings.DEBUG:
