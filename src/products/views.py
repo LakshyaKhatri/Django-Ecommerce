a2de2b78a41686bc653c6c8f7ce01f02ed767924
LakshyaKhatri <lakshyak67@gmail.com>
2019-01-20 10:41:19 +0530

    Add product details page

--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 # Create your views here.
@@ -10,3 +10,11 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+
+def product_detail_view(request, pk):
+    instance = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
