d99abfe3c342d24307379532db7a300763260d91
LakshyaKhatri <lakshyak67@gmail.com>
2019-02-13 18:28:58 +0530

    Add image fiedls to product details

--- products/models.py
+++ products/models.py
@@ -1,13 +1,27 @@
 from django.db import models
-
+import random
+import os
 # Create your models here.
+
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = "{new_filename}{ext}".format(new_filename=new_filename, ext=ext)
+    return "products/{final_filename}".format(final_filename=final_filename)
 
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=00.0)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.title
